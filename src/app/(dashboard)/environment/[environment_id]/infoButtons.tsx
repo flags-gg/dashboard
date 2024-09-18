ae276e0f76e2e35a0497e850be81acf70b6f219b
Keloran <keloran@develbox.info>
2024-09-18 22:52:28 +0100

    build: Update dependencies and add new environment variable "UPLOADTHING_TOKEN".

--- src/app/(dashboard)/environment/[environment_id]/infoButtons.tsx
+++ src/app/(dashboard)/environment/[environment_id]/infoButtons.tsx
@@ -4,7 +4,6 @@ import {type Session} from "next-auth";
 import CreateFlag from "./flags/create";
 import {buttonVariants} from "~/components/ui/button";
 import Guide from './guide';
-// @ts-expect-error types missing
 import {useFlags} from "@flags-gg/react-library";
 import {CardFooter} from "~/components/ui/card";
 import Link from "next/link";
@@ -14,9 +13,9 @@ export default function InfoButtons({session, environmentId, menuId}: {session: 
 
   return (
     <CardFooter className={"p-3 border-t-2 gap-2 items-center justify-center"}>
-      {is("createFlag").enabled() && <CreateFlag session={session} environment_id={environmentId} />}
-      {is("viewGuide").enabled() && <Guide session={session} />}
-      {is("secret menu").enabled() && (
+      {is("createFlag")?.enabled() && <CreateFlag session={session} environment_id={environmentId} />}
+      {is("viewGuide")?.enabled() && <Guide session={session} />}
+      {is("secret menu")?.enabled() && (
         menuId ? (
           <Link className={buttonVariants({variant: "default"})} href={`/secretmenu/${menuId}`}>Secret Menu</Link>
         ) : (
--- src/env.js
+++ src/env.js
@@ -28,6 +28,7 @@ export const env = createEnv({
     KEYCLOAK_ISSUER: z.string(),
 
     UPLOADTHING_SECRET: z.string(),
+    UPLOADTHING_TOKEN: z.string(),
     UPLOADTHING_APP_ID: z.string(),
 
     STRIPE_KEY: z.string(),
@@ -62,6 +63,7 @@ export const env = createEnv({
     KEYCLOAK_ISSUER: process.env.KEYCLOAK_ISSUER,
 
     UPLOADTHING_SECRET: process.env.UPLOADTHING_SECRET,
+    UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     UPLOADTHING_APP_ID: process.env.UPLOADTHING_APP_ID,
 
     STRIPE_KEY: process.env.STRIPE_KEY,
