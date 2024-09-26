a7503c7cff1fb24b688a10cbf2cb6093ec075f02
Keloran <keloran@develbox.info>
2024-09-26 15:59:30 +0100

    refactor: Update next-auth to version 4.24.8

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,4 @@ export default {
     "^.+.tsx?$": ["ts-jest",{}],
   },
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-};/** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
 };
--- src/app/api/flag/list/route.ts
+++ src/app/api/flag/list/route.ts
@@ -28,34 +28,4 @@ export async function POST(request: Request) {
         console.error('Failed to fetch flags', e)
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
     }
-}import { env } from "~/env";
-import { NextResponse } from "next/server";
-
-type GetFlags = {
-    sessionToken: string
-    userId: string
-    environment_id: string
-}
-
-export async function POST(request: Request) {
-    const { environment_id, sessionToken, userId }: GetFlags = await request.json() as GetFlags
-    const apiUrl = `${env.API_SERVER}/environment/${environment_id}/flags`
-
-    try {
-        const res = await fetch(apiUrl, {
-            headers: {
-                'x-user-access-token': sessionToken,
-                'x-user-subject': userId,
-            },
-            cache: 'no-store'
-        })
-        if (!res.ok) {
-            return NextResponse.json({ message: "Failed to fetch flags" }, { status: 500 });
-        }
-
-        return NextResponse.json(await res.json())
-    } catch (e) {
-        console.error('Failed to fetch flags', e)
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
-    }
 }
