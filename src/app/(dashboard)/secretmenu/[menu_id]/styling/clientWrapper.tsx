c0f70d326246dd6c0ec190dc38c29efb33267310
Keloran <keloran@develbox.info>
2024-10-04 23:41:40 +0100

    feat(styling): Add custom styles fetching and using in secretmenu pages

--- src/app/(dashboard)/secretmenu/[menu_id]/styling/clientWrapper.tsx
+++ src/app/(dashboard)/secretmenu/[menu_id]/styling/clientWrapper.tsx
@@ -1,21 +1,53 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from "react";
 import Info from "./info";
 import { type Session } from "next-auth";
 import { StyleProvider } from "./context";
 import PageContainer from "./pageContainer";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useStyles } from "~/hooks/use-styles";
+import { useToast } from "~/hooks/use-toast";
 
 type ClientWrapperProps = {
   session: Session;
   menuId: string;
 };
 
-export default function ClientWrapper({ session, menuId }: ClientWrapperProps) {
+function StyleWrapper({ session, menuId }: ClientWrapperProps) {
+  const { data, isLoading, error } = useStyles(session, menuId);
+  const { toast } = useToast();
+
+  if (isLoading) {
+    return <div className="col-span-2 gap-3">Loading...</div>;
+  }
+
+  if (error) {
+    toast({
+      title: "Error loading styles",
+      description: "Please try again later.",
+    });
+    return <div className="col-span-2 gap-3">Error loading styles</div>;
+  }
+
+  if (!data) {
+    throw new Error("No data returned");
+  }
+
   return (
-    <StyleProvider>
-      <PageContainer session={session} menuId={menuId} />
+    <StyleProvider initialStyles={data.styles}>
+      <PageContainer />
       <Info session={session} menuId={menuId} />
     </StyleProvider>
+  );
+}
+
+export default function ClientWrapper({ session, menuId }: ClientWrapperProps) {
+  const [queryClient] = useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <StyleWrapper session={session} menuId={menuId} />
+    </QueryClientProvider>
   );
 }
--- src/app/api/secretmenu/style/route.ts
+++ src/app/api/secretmenu/style/route.ts
@@ -1,17 +1,14 @@
 import { env } from "~/env";
 import { NextResponse } from "next/server";
-import { StyleState } from "~/app/(dashboard)/secretmenu/[menu_id]/styling/context";
 
-type StyleParams = {
-  sessionToken: string
-  userId: string
-  menuId: string
-  style: string
-}
+export async function POST(request: Request) {
+  type StyleParams = {
+    sessionToken: string
+    userId: string
+    menuId: string
+  }
 
-export async function POST(request: Request): Promise<StyleState | Error> {
   const { sessionToken, userId, menuId }: StyleParams = await request.json() as StyleParams
-
   const apiUrl = `${env.API_SERVER}/secret-menu/${menuId}/style`
   const response = await fetch(apiUrl, {
     method: 'GET',
@@ -23,16 +20,42 @@ export async function POST(request: Request): Promise<StyleState | Error> {
     cache: 'no-store',
   })
   if (!response.ok) {
-    return new Error('Failed to get style')
+    console.error("Failed to get style", response)
+    return NextResponse.json({ message: "Failed to get style" }, { status: 500 })
   }
 
-  return await response.json() as StyleState
+  const data = await response.json()
+  return NextResponse.json(data)
 }
 
 export async function PUT(request: Request) {
-  const { sessionToken, userId, menuId, style }: StyleParams = await request.json() as StyleParams
+  type StyleParams = {
+    sessionToken: string
+    userId: string
+    menuId?: string
+    styleId?: string
+    style: string
+  }
 
+  const { sessionToken, userId, menuId, styleId, style }: StyleParams = await request.json() as StyleParams
   const styleJson = JSON.parse(style)
+  const dataModel = {
+    menu_id: menuId,
+    custom_style: {
+      style_id: "",
+      reset_button: styleJson.resetButton,
+      close_button: styleJson.closeButton,
+      container: styleJson.container,
+      flag: styleJson.flag,
+      button_enabled: styleJson.buttonEnabled,
+      button_disabled: styleJson.buttonDisabled,
+      header: styleJson.header,
+    }
+  }
+
+  if (styleId) {
+    dataModel.custom_style.style_id = styleId
+  }
 
   try {
     const apiUrl = `${env.API_SERVER}/secret-menu/${menuId}/style`
@@ -43,18 +66,7 @@ export async function PUT(request: Request) {
         'x-user-access-token': sessionToken,
         'x-user-subject': userId,
       },
-      body: JSON.stringify({
-        menu_id: menuId,
-        custom_style: {
-          reset_button: styleJson.resetButton,
-          close_button: styleJson.closeButton,
-          container: styleJson.container,
-          flag: styleJson.flag,
-          button_enabled: styleJson.buttonEnabled,
-          button_disabled: styleJson.buttonDisabled,
-          header: styleJson.header,
-        },
-      }),
+      body: JSON.stringify(dataModel),
       cache: 'no-store',
     })
     if (!response.ok) {
