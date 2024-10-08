71c9b4dfe5a147621f4633f9f3b3dbc7daa02d0f
Keloran <keloran@develbox.info>
2024-10-08 09:49:07 +0100

    feat: Update SearchBox component to disable input based on flag status

--- src/app/components/HeaderBar/search.tsx
+++ src/app/components/HeaderBar/search.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import {Search} from "lucide-react";
 import {Input} from "~/components/ui/input";
+import { useFlags } from "@flags-gg/react-library";
 
 export function SearchBox() {
+  const {is} = useFlags();
+
   return (
     <div className={"relative ml-auto flex-1 md:grow-0"}>
-      <Search className={"absolute left-2.5 top-2.5 h-4 w-5 text-muted-foreground"} />
-      <Input type="search" placeholder="Search" className={"w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"} />
+      <Search className={"absolute left-2.5 top-3 h-4 w-5 text-muted-foreground"} />
+      <Input type="search" placeholder="Search" className={"w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"} disabled={!is("search")?.enabled()}/>
     </div>
   )
 }
