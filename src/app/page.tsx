1c736c9e3a161aaa2fe5c661685edeb0a013d9a1
Keloran <keloran@develbox.info>
2024-10-13 18:33:02 +0100

    feat: Add console logging for environment variable 'env'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {redirect} from "next/navigation";
 
 import { getServerAuthSession } from "~/server/auth";
+import { env } from "~/env";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -8,6 +9,8 @@ export default async function Home() {
   if (!session) {
     redirect('/login')
   }
+
+  console.info("env", env)
 
   return (
     <>Dashboard</>
