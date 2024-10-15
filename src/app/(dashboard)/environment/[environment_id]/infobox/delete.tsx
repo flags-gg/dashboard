a2eeecfad01977855aa850b9173e00d90a897ea9
Keloran <keloran@develbox.info>
2024-10-15 20:18:54 +0100

    ✨ Remove Clone component from InfoBox titles and update Delete component styling.

--- src/app/(dashboard)/environment/[environment_id]/infobox/delete.tsx
+++ src/app/(dashboard)/environment/[environment_id]/infobox/delete.tsx
@@ -4,19 +4,19 @@ import { useFlags } from "@flags-gg/react-library";
 import { environmentAtom } from "~/lib/statemanager";
 import { useAtom } from "jotai";
 import { useState } from "react";
-import { Copy } from "lucide-react";
+import { Trash2 } from "lucide-react";
 import { Button } from "~/components/ui/button";
 import { Popover, PopoverContent, PopoverTrigger } from "~/components/ui/popover";
 import { Tooltip, TooltipContent, TooltipTrigger } from "~/components/ui/tooltip";
 
-export default function Clone({environment_id}: {environment_id: string}) {
+export default function Delete({environment_id}: {environment_id: string}) {
   const [environmentInfo] = useAtom(environmentAtom)
   const [openClone, setOpenClone] = useState(false);
   const {is} = useFlags();
 
   console.info("clone environmentInfo", environmentInfo, environment_id)
 
-  if (!is("clone env")?.enabled()) {
+  if (!is("delete env")?.enabled()) {
     return <></>
   }
 
@@ -28,7 +28,7 @@ export default function Clone({environment_id}: {environment_id: string}) {
             <Button variant={"outline"} className={"bg-muted/10 border-0"} size={"icon"} style={{
               marginTop: "-0.4rem",
             }}>
-              <Copy className={"h-5 w-5"} />
+              <Trash2 className={"h-5 w-5"} />
             </Button>
           </TooltipTrigger>
           <TooltipContent sideOffset={4}>Clone Environment</TooltipContent>
--- src/app/(dashboard)/environment/[environment_id]/infobox/index.tsx
+++ src/app/(dashboard)/environment/[environment_id]/infobox/index.tsx
@@ -4,6 +4,8 @@ import Info from "./info";
 import Name from "./name";
 import {InfoBoxError} from "~/app/components/InfoBoxError";
 import InfoButtons from "./buttons";
+import Clone from "./clone"
+import Delete from "./delete"
 
 export default async function InfoBox({environment_id}: {environment_id: string}) {
   const { data: environmentInfo, error } = await getEnvironment(environment_id)
@@ -17,6 +19,8 @@ export default async function InfoBox({environment_id}: {environment_id: string}
     <Card>
       <CardHeader className={"flex flex-row items-start bg-muted/50"}>
         <Name environment_id={environment_id} />
+        <Clone environment_id={environment_id} />
+        <Delete environment_id={environment_id} />
       </CardHeader>
       <CardContent className={"p-6 text-sm"}>
         <Info environmentInfo={environmentInfo} />
