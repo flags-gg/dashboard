6f608e20ce90b4ab2c71a67b105cf28c3aa2affc
Keloran <keloran@develbox.info>
2024-10-21 10:46:42 +0100

    ✨ Add search functionality with project, agent, and environment data prefetching.

---
+++ src/app/api/project/list/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { type IProject } from "~/lib/statemanager";
+import { env } from "~/env";
+import { getServerAuthSession } from "~/server/auth";
+
+export async function GET() {
+  const session = await getServerAuthSession();
+  if (!session?.user?.access_token) {
+    return new NextResponse('Unauthorized', { status: 401 })
+  }
+
+  try {
+    const response = await fetch(`${env.API_SERVER}/projects`, {
+      method: 'GET',
+      headers: {
+        'x-user-access-token': session.user.access_token,
+        'x-user-subject': session.user.id,
+      },
+      cache: 'no-store',
+    })
+    if (!response.ok) {
+      return NextResponse.json({ message: 'Failed to fetch projects' }, { status: 500 })
+    }
+
+    const data = await response.json() as IProject[]
+    return NextResponse.json(data)
+  } catch (e) {
+    console.error('Failed to fetch projects', e)
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
+  }
+}
--- src/app/components/HeaderBar/search.tsx
+++ src/app/components/HeaderBar/search.tsx
@@ -12,42 +12,150 @@ import {
 import { useState } from "react";
 import { Search } from "lucide-react";
 import { Input } from "~/components/ui/input";
+import { type AgentsData, type EnvironmentsData, type ProjectsData } from "~/lib/statemanager";
+import { useQuery } from "@tanstack/react-query";
+import { useToast } from "~/hooks/use-toast";
+
+async function getProjects() {
+  const res = await fetch(`/api/project/list`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch projects");
+  }
+
+  return await res.json() as ProjectsData;
+}
+
+async function getAgents() {
+  const res = await fetch(`/api/agent/list`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch agents");
+  }
+
+  return await res.json() as AgentsData;
+}
+
+async function getEnvironments() {
+  const res = await fetch(`/api/environment/list`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch environments");
+  }
+
+  return await res.json() as EnvironmentsData;
+}
 
 export function SearchBox() {
+  const {toast} = useToast();
   const {is} = useFlags();
   const [isOpen, setIsOpen] = useState(false);
+  const {data: projectsData, error: projectsError} = useQuery({
+    queryKey: ["projects"],
+    queryFn: getProjects,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+  });
+  const {data: agentsData, error: agentsError} = useQuery({
+    queryKey: ["agents"],
+    queryFn: getAgents,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+  });
+  const {data: environmentsData, error: environmentsError} = useQuery({
+    queryKey: ["environments"],
+    queryFn: getEnvironments,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+  });
 
   if (!is("search")?.enabled()) {
     return <div className={"relative ml-auto flex-1 md:grow-0"}>
     </div>
   }
 
+  if (projectsError || agentsError || environmentsError) {
+    toast({
+      title: "Error loading search data",
+      description: "Please try again later.",
+    });
+  }
+
   return (
     <div className={"relative ml-auto flex-1 md:grow-0"}>
       <div onClick={() => setIsOpen(true)}>
-        <Search className={"absolute left-2.5 top-3 h-4 w-5 text-muted-foreground"} />
-        <Input type="search" placeholder="Search" className={"w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"} />
+        <Search
+          className={"absolute left-2.5 top-3 h-4 w-5 text-muted-foreground"}
+        />
+        <Input
+          type="search"
+          placeholder="Search"
+          className={
+            "w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+          }
+        />
       </div>
 
       <CommandDialog open={isOpen} onOpenChange={setIsOpen}>
-        <CommandInput placeholder="Search" className={"w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"} />
+        <CommandInput
+          placeholder="Search"
+          className={
+            "w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+          }
+        />
         <CommandList>
           <CommandEmpty>No Results</CommandEmpty>
           <CommandGroup heading="Projects">
-            <CommandItem>Project 1</CommandItem>
+            {projectsData?.projects?.map((project) => (
+              <CommandItem key={project.project_id} onSelect={() => {
+                setIsOpen(false)
+                window.location.href = `/project/${project?.project_id}`
+              }}><span className={"font-bold"}>{project?.name}</span></CommandItem>
+            ))}
           </CommandGroup>
-          <CommandGroup heading="Agents">
-            <CommandItem>Agent 1</CommandItem>
-          </CommandGroup>
-          <CommandGroup heading="Environments">
-            <CommandItem>Environment 1</CommandItem>
-          </CommandGroup>
-          <CommandGroup heading="Secret Menus">
-            <CommandItem>Secret Menu 1</CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
+          {agentsData && (
+            <>
+              <CommandSeparator />
+              <CommandGroup heading="Agents">
+                {agentsData?.agents?.map((agent) => (
+                  <CommandItem key={agent?.agent_id} onSelect={() => {
+                    setIsOpen(false)
+                    window.location.href = `/agent/${agent?.agent_id}`
+                  }}>[<span className={"font-italic"}>{agent?.project_info?.name}</span>]&nbsp;<span className={"font-bold"}>{agent?.name}</span></CommandItem>
+                ))}
+              </CommandGroup>
+            </>
+          )}
+          {environmentsData && (
+            <>
+              <CommandSeparator />
+              <CommandGroup heading="Environments">
+                {environmentsData?.environments?.map((environment) => (
+                  <CommandItem key={environment?.environment_id} onSelect={() => {
+                    setIsOpen(false)
+                    window.location.href = `/environment/${environment?.environment_id}`
+                  }}>[<span className={"font-italic"}>{environment?.project_name}: {environment?.agent_name}</span>]&nbsp;<span className={"font-bold"}>{environment?.name}</span></CommandItem>
+                ))}
+              </CommandGroup>
+            </>
+          )}
         </CommandList>
       </CommandDialog>
     </div>
-  )
+  );
 }
