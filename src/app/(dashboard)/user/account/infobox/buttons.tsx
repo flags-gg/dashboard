4dfeec0470c17932e930cad0e9abc22303e11ca5
Keloran <keloran@develbox.info>
2024-11-06 13:37:11 +0000

    ✨ feat: add account deletion functionality and UI updates

--- src/app/(dashboard)/user/account/infobox/buttons.tsx
+++ src/app/(dashboard)/user/account/infobox/buttons.tsx
@@ -1,9 +1,87 @@
-import { CardFooter } from "~/components/ui/card";
+"use client"
 
-export async function InfoButtons() {
+import { CardFooter } from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import { useToast } from "~/hooks/use-toast";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription, DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "~/components/ui/dialog";
+
+async function deleteAccount(): Promise<null | Error> {
+  try {
+    const response = await fetch(`/api/user/delete`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      cache: "no-store",
+    });
+    if (!response.ok) {
+      return new Error("Failed to delete account");
+    }
+
+    return null;
+  } catch (error) {
+    if (error instanceof Error) {
+      return Error(`Failed to delete account: ${error.message}`);
+    } else {
+      console.error("deleteAccount", error);
+    }
+  }
+
+  return new Error("Failed to delete account");
+}
+
+export function InfoButtons() {
+  const [openDelete, setOpenDelete] = useState(false);
+  const {toast} = useToast();
+
   return (
     <CardFooter className="p-3 border-t-2 gap-2 items-center justify-center">
-      Delete Account
+      <Dialog open={openDelete} onOpenChange={setOpenDelete}>
+        <DialogTrigger asChild>
+          <Button variant="destructive" className="w-full">
+            Delete Account
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Account</DialogTitle>
+            <DialogDescription>Are you sure you want to delete your account?</DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button onClick={() => setOpenDelete(false)}>Cancel</Button>
+            <Button variant="destructive" onClick={() => {
+              setOpenDelete(false)
+              deleteAccount().then(() => {
+                toast({
+                  title: "Account deleted",
+                  description: "Your account has been deleted",
+                });
+              }).catch((error) => {
+                if (error instanceof Error) {
+                  toast({
+                    title: "Failed to delete account",
+                    description: error.message,
+                  });
+                } else {
+                  console.error("deleteAccount", error);
+                  toast({
+                    title: "Failed to delete account",
+                    description: "Failed to delete account",
+                  });
+                }
+              });
+            }}>Delete</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </CardFooter>
   );
 }
