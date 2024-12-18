f9af6172b956193be051b6960ae8d26038e3fd37
Keloran <keloran@develbox.info>
2024-12-18 15:10:10 +0000

    ✨ refactor: remove unused imports and error handling logic

--- src/app/(dashboard)/company/settings/index.tsx
+++ src/app/(dashboard)/company/settings/index.tsx
@@ -2,43 +2,16 @@
 
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
 import { useFlags } from "@flags-gg/react-library";
-import { useCompanyDetails } from "~/hooks/use-company-details";
-import { useState } from "react";
-import { useToast } from "~/hooks/use-toast";
-import { Skeleton } from "~/components/ui/skeleton";
 
 import Plan from "./plan";
 import DeleteCompany from "./delete"
 import Details from "./details"
 
-interface IError {
-  message: string
-  title: string
-}
-
 export default function Settings() {
   const {is} = useFlags();
-  const {data: companyInfo, isLoading} = useCompanyDetails();
-  const [upgradeOpen, setUpgradeOpen] = useState(false);
-  const {toast} = useToast();
-  const [errorInfo, setErrorInfo] = useState({} as IError);
-  const [showError, setShowError] = useState(false);
-
 
   if (!is("company settings")?.enabled()) {
     return <></>
-  }
-
-  if (isLoading) {
-    return <Skeleton className="min-h-[10rem] min-w-fit rounded-xl" />
-  }
-
-  if (showError) {
-    toast({
-      title: errorInfo.title,
-      description: errorInfo.message,
-      duration: 5000,
-    })
   }
 
   return (
