34748acbc3d89d5a96665e3e934561f901cd6a2f
Keloran <keloran@develbox.info>
2024-12-24 21:53:36 +0000

    ✨ refactor: streamline state management and interfaces

--- src/app/api/company/pricing/route.ts
+++ src/app/api/company/pricing/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import { env } from "~/env";
 import { getServerAuthSession } from "~/server/auth";
-import { ICompanyInfo } from "~/lib/interfaces";
+import { env } from "~/env";
 
 export async function GET() {
   const session = await getServerAuthSession();
@@ -10,7 +9,7 @@ export async function GET() {
   }
 
   try {
-    const response = await fetch(`${env.API_SERVER}/company`, {
+    const response = await fetch(`${env.API_SERVER}/company/pricing`, {
       method: 'GET',
       headers: {
         'x-user-access-token': session.user.access_token,
@@ -19,12 +18,12 @@ export async function GET() {
       cache: 'no-store',
     })
     if (!response.ok) {
-      return NextResponse.json({ message: 'Failed to fetch company info' }, { status: 500 })
+      return NextResponse.json({ message: 'Failed to fetch company limits' }, { status: 500 })
     }
 
-    return NextResponse.json(await response.json() as ICompanyInfo)
+    return NextResponse.json(await response.json())
   } catch (e) {
-    console.error('Failed to fetch company info', e)
+    console.error('Failed to fetch company limits', e)
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 })
   }
 }
---
+++ src/hooks/use-upgrade-choices.ts
@@ -0,0 +1,42 @@
+import { useAtom } from "jotai";
+import { commitHashAtom } from "~/lib/statemanager";
+import { useQuery } from "@tanstack/react-query";
+
+interface UpgradeChoice {
+  title: string;
+  agents: number;
+  environments: number;
+  team_members: number;
+  price?: number;
+}
+interface UpgradeChoices {
+  prices: UpgradeChoice[];
+}
+
+const fetchUpgradeChoices = async (): Promise<UpgradeChoices> => {
+  const res = await fetch(`/api/company/pricing`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    console.error('Failed to fetch company limits', res.status, res.statusText);
+    throw new Error('Failed to fetch company limits');
+  }
+
+  return await res.json() as UpgradeChoices;
+};
+
+export const useUpgradeChoices = () => {
+  const [commitHash] = useAtom(commitHashAtom)
+
+  return useQuery<UpgradeChoices, Error>({
+    queryKey: ['companyUpgradeChoices', commitHash],
+    queryFn: () => fetchUpgradeChoices(),
+    retry: 3,
+    staleTime: 5 * 60 * 1000, // 5 minutes
+  });
+};
