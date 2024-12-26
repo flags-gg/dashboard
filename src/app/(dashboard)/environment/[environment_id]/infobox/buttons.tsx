dc4686cb6c943b5ef95bf18afa3aadf90de8cf6c
Keloran <keloran@develbox.info>
2024-12-26 23:40:06 +0000

    ✨ refactor: remove unused Guide component and clean up code

--- src/app/(dashboard)/environment/[environment_id]/infobox/buttons.tsx
+++ src/app/(dashboard)/environment/[environment_id]/infobox/buttons.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import CreateFlag from "../flags/create";
-import Guide from './guide';
 import {useFlags} from "@flags-gg/react-library";
 import {CardFooter} from "~/components/ui/card";
 
@@ -11,7 +10,6 @@ export default function InfoButtons({environmentId}: {environmentId: string}) {
   return (
     <CardFooter className={"p-3 border-t-2 gap-2 items-center justify-center "}>
       <CreateFlag environment_id={environmentId} />
-      {is("view guide")?.enabled() && <Guide />}
     </CardFooter>
   );
 }
